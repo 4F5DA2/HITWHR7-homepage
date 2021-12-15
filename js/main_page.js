af3208e16e146cc9ce550b0e7003a1f3a14e324a
4F5DA2 <91315626+4F5DA2@users.noreply.github.com>
2021-12-15 21:39:29 +0800

    Add files via upload

--- js/main_page.js
+++ js/main_page.js
@@ -2,11 +2,11 @@
 function print_weather(){
 	var city_name = returnCitySN["cname"];
 	if(city_name == "CHINA")
-		{
-			document.getElementById("weather_page").innerHTML = "暂时无法获得该地区天气信息，请更换网络后重新查询";
-		}
-		else
-		{
-			document.getElementById("weather_page").innerHTML = "暂时无法获得该地区天气信息，请更换网络后重新查询";
-		}
+	{
+		document.getElementById("weather_page").innerHTML = "暂时无法获得该地区天气信息，请更换网络后重新查询";
+	}
+	else
+	{
+		document.getElementById("weather_page").innerHTML = "暂时无法获得该地区天气信息，请更换网络后重新查询";
+	}
 }
